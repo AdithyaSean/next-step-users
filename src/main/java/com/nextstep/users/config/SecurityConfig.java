c23fd79b41a11da36f40d8dc9831ede737fce471
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-22 21:19:42 +0530

    Authentication endpoint for next-step-users ms

--- src/main/java/com/nextstep/users/config/SecurityConfig.java
+++ src/main/java/com/nextstep/users/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
